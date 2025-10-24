388482159564b00349b12394913828df824c76a3
devclaudioalmeida <claudio.almeida.webmarketing@gmail.com>
2025-10-24 12:55:20 -0300

    Mais um pouco de prática

--- CAP_9/classes_01.py
+++ CAP_9/classes_01.py
@@ -1,4 +1,4 @@
-#Exercícios 9.1 e 9.2, 9.4
+#Exercícios 9.1, 9.2, 9.4, 9.6
 
 class Restaurante:
 
@@ -28,7 +28,19 @@ class Restaurante:
     
     def ler_num_servidos(self):
         #le o numero de pessoas servidas no restaurante
-        print(f'Foram servidas {self.num_servidos} pessoas no restaurante {self.nome}!')
+        print(f'{self.nome} já serviu {self.num_servidos} pessoas!')
+
+#Definição da classe Sorveteria, que é filha de Restaurante
+class Sorveteria (Restaurante):
+    def __init__(self, nome, tipo):
+        #Inicializa os atributos da Classe Pai (Restaurante)
+        super().__init__(nome, tipo)
+        self.sabores = ['morango', 'uva', 'chocolate', 'baunilha']
+    def get_sabores(self):
+        print(f'A sorveteria {self.nome} tem os seguintes sabores disponíveis:')
+        for sabor in self.sabores:
+            print(f' - {sabor}')
+
 
 
 
@@ -38,6 +50,7 @@ rest_arabe.restaurante_aberto()
 rest_italiano = Restaurante('Delicias da Nona', 'Italiana')
 rest_japones = Restaurante('SUSHITAKARO', 'Japonesa')
 rest_mexicano = Restaurante('Arriba Burritos', 'Mexicana')
+sov_01 = Sorveteria('Gostosuras FIT', 'Light')
 rest_japones.restaurante_info()
 rest_italiano.restaurante_info()
 rest_mexicano.restaurante_info()
@@ -49,3 +62,7 @@ rest_japones.set_num_served(3)
 rest_japones.ler_num_servidos()
 rest_japones.incremeta_num_servidos(2)
 rest_japones.ler_num_servidos()
+print('+' *60)
+sov_01.incremeta_num_servidos (1)
+sov_01.get_sabores()
+sov_01.ler_num_servidos()
