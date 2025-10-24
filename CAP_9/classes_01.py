29e5695217e9226c3d60b9bb1f1dcdc914310086
devclaudioalmeida <claudio.almeida.webmarketing@gmail.com>
2025-10-24 11:29:51 -0300

    Continuação do capítulo 9
    Continuação do estudo de POO do curso intensivo de Python.

--- CAP_9/classes_01.py
+++ CAP_9/classes_01.py
@@ -1,4 +1,4 @@
-#Exercícios 9.1 e 9.2
+#Exercícios 9.1 e 9.2, 9.4
 
 class Restaurante:
 
@@ -7,6 +7,7 @@ class Restaurante:
         #Inicializa os atributos nome e tipo
         self.nome = nome
         self.tipo = tipo
+        self.num_servidos = 0
     
     def restaurante_info(self):
         #Exibe uma mensagem informando o tipo de cozinha do restaurante
@@ -15,6 +16,20 @@ class Restaurante:
     def restaurante_aberto(self):
         #Exibe uma mensagem informando que o restaurante está aberto
         print(f'O restaurante {self.nome} está aberto!')
+
+    #A partir daqui é a solução do exerrcício 9.4
+    def set_num_served(self, numero):
+        #Seta o número de pessoas servidas no restaurante
+        self.num_servidos = numero
+
+    def incremeta_num_servidos(self, inc):
+        #incrementa o número de pessoas servidas no restaurante
+        self.num_servidos += inc
+    
+    def ler_num_servidos(self):
+        #le o numero de pessoas servidas no restaurante
+        print(f'Foram servidas {self.num_servidos} pessoas no restaurante {self.nome}!')
+
 
 
 rest_arabe = Restaurante('Sabores Internacional', 'Arabe')
@@ -26,4 +41,11 @@ rest_mexicano = Restaurante('Arriba Burritos', 'Mexicana')
 rest_japones.restaurante_info()
 rest_italiano.restaurante_info()
 rest_mexicano.restaurante_info()
-
+print('+' *60)
+rest_japones.ler_num_servidos()
+rest_japones.num_servidos = 1
+rest_japones.ler_num_servidos()
+rest_japones.set_num_served(3)
+rest_japones.ler_num_servidos()
+rest_japones.incremeta_num_servidos(2)
+rest_japones.ler_num_servidos()
--- CAP_9/classes_02.py
+++ CAP_9/classes_02.py
@@ -1,9 +1,10 @@
-#Exercício 9.3
+#Exercício 9.3, 9.5
 class Usuario:
     def __init__(self, nome, sobrenome):
         #Inicializa os atributos nome e sobrenome
         self.nome = nome
         self.sobrenome = sobrenome
+        self.tentativas_login = 0
     
     def descricao_usuario(self):
         #Exibe na tela o nome e o sobrenome do usuário
@@ -12,6 +13,21 @@ class Usuario:
     def cumprimenta_usuario(self):
         #Exibe uma mensagem de boas vindas ao usuário
         print(f'Seja bem vindo {self.nome}')
+
+    #A partir daqui começa a solução do exercício 9.5
+    def incremeta_tentativas_login(self):
+        #incrementa as tentativas de login em 1 unidade
+        self.tentativas_login += 1
+    
+    def reset_tentativas_login(self):
+        #Reinicia o contador de tentativas de login
+        self.tentativas_login = 0
+    
+    def get_tentativas_login(self):
+        #Retorna o número de tentativas de login
+        return self.tentativas_login
+
+
 
 
 usuario_01 = Usuario('Cláudio', 'Almeida')
@@ -25,3 +41,14 @@ print('*' * 60)
 usuario_01.cumprimenta_usuario()
 usuario_02.cumprimenta_usuario()
 usuario_03.cumprimenta_usuario()
+print('*' * 60)
+print(f'O usuário {usuario_02.nome} tentou logar {usuario_02.get_tentativas_login()} vezes no sistema!')
+usuario_02.incremeta_tentativas_login()
+usuario_02.incremeta_tentativas_login()
+usuario_02.incremeta_tentativas_login()
+usuario_02.incremeta_tentativas_login()
+print('*' * 60)
+print(f'O usuário {usuario_02.nome} tentou logar {usuario_02.get_tentativas_login()} vezes no sistema!')
+usuario_02.reset_tentativas_login()
+print('*' * 60)
+print(f'O usuário {usuario_02.nome} tentou logar {usuario_02.get_tentativas_login()} vezes no sistema!')
